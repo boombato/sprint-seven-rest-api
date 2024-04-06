c160ddce3c939928528ad44c3758e311776ddae3
Aleksandr Gorn <boombato@gmail.com>
2024-04-06 08:59:41 +0300

    first try

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Task struct {
+	ID           string   `json:"id"`          // ID задачи
+	Description  string   `json:"description"` // Заголовок
+	Note         string   `json:"note"`        // Описание задачи
+	Applications []string `json:"application"` // Приложения, которыми будете пользоваться
+}
+
+var tasks = map[string]Task{
+	"1": {
+		ID:          "1",
+		Description: "Сделать финальное задание темы REST API",
+		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+		},
+	},
+	"2": {
+		ID:          "2",
+		Description: "Протестировать финальное задание с помощью Postmen",
+		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
+		Applications: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+			"Postman",
+		},
+	},
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	// сериализуем данные из слайса artists
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// в заголовок записываем тип контента, у нас это данные в формате JSON
+	w.Header().Set("Content-Type", "application/json")
+	// так как все успешно, то статус OK
+	w.WriteHeader(http.StatusOK)
+	// записываем сериализованные в JSON данные в тело ответа
+	w.Write(resp)
+}
+
+func postTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+	delete(tasks, id)
+
+	/*	resp, err := json.Marshal(task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(resp) */
+
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/tasks", getTasks)
+	r.Post("/tasks", postTask)
+	r.Get("/tasks/{id}", getTask)
+	r.Delete("/tasks/{id}", deleteTask)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
